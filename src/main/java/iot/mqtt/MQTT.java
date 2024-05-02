d3a9fd95da7dfa7304661cd9e17b2e776e75dea7
YanSchw <schoessow.yannick@gmail.com>
2024-05-03 00:16:28 +0200

    Update MQTT.java

--- src/main/java/iot/mqtt/MQTT.java
+++ src/main/java/iot/mqtt/MQTT.java
@@ -5,21 +5,23 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class MQTT {
 
+    private static MqttClient mqttClient;
+
     public static void init() {
 
-        String topic        = "#";
-        int qos             = 2;
-        String broker       = "tcp://141.41.35.170:1883";
-        String clientId     = "Java";
+        final String topic        = "#";
+        final int qos             = 2;
+        final String broker       = "tcp://141.41.35.170:1883";
+        final String clientId     = "Java";
         MemoryPersistence persistence = new MemoryPersistence();
 
         try {
-            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            mqttClient = new MqttClient(broker, clientId, persistence);
             MqttConnectOptions connOpts = new MqttConnectOptions();
             connOpts.setCleanSession(true);
-            System.out.println("Connecting to broker: "+broker);
-            sampleClient.connect(connOpts);
-            sampleClient.subscribe(topic, qos, new IMqttMessageListener() {
+            System.out.println("Connecting to broker: " + broker);
+            mqttClient.connect(connOpts);
+            mqttClient.subscribe(topic, qos, new IMqttMessageListener() {
                 @Override
                 public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
                     System.out.println(mqttMessage.toString());
@@ -27,10 +29,13 @@ public class MQTT {
             });
             System.out.println("Subscribed");
 
-            // LOOP FOREVER
-            //while (sampleClient.isConnected());
-
-            //System.out.println("Shutdown");
+        } catch(MqttException me) {
+            me.printStackTrace();
+        }
+    }
+    public static void activatePump(int duration) {
+        try {
+            mqttClient.publish("event/pump", String.valueOf(duration).getBytes(), 2, false);
         } catch(MqttException me) {
             me.printStackTrace();
         }
