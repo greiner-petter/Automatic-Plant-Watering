c8bd9726017e443e6ef655fbdc29e2f23b551706
YanSchw <schoessow.yannick@gmail.com>
2024-05-09 14:28:55 +0200

    Added Altitude & Pressure

--- src/main/java/iot/influx/Influx.java
+++ src/main/java/iot/influx/Influx.java
@@ -18,21 +18,30 @@ public class Influx {
         String org = "my-org";
         String bucket = "telegraf";
         influxDBClient = InfluxDBClientFactory.create(url, token.toCharArray(), org, bucket);
-        System.out.println("Influx Ping: " +  influxDBClient.ping());
+        System.out.println("Influx Ping: " + influxDBClient.ping());
     }
 
     public static List<FluxRecord> getTemps() {
+        return queryTopic("devices/project/temp");
+    }
+
+    public static List<FluxRecord> getAltitude() {
+        return queryTopic("devices/project/altitude");
+    }
+    public static List<FluxRecord> getPressure() {
+        return queryTopic("devices/project/pressure");
+    }
+    private static List<FluxRecord> queryTopic(String topic) {
         String flux = "from(bucket: \"telegraf\")\n" +
                 "  |> range(start: -1h)\n" +
                 "  |> filter(fn: (r) => r[\"_measurement\"] == \"mqtt_consumer\")\n" +
                 "  |> filter(fn: (r) => r[\"_field\"] == \"value\")\n" +
                 "  |> filter(fn: (r) => r[\"host\"] == \"289b3f2187aa\")\n" +
-                "  |> filter(fn: (r) => r[\"topic\"] == \"devices/project/temp\")\n" +
+                "  |> filter(fn: (r) => r[\"topic\"] == \""+topic+"\")\n" +
                 "  |> aggregateWindow(every: 1m, fn: mean, createEmpty: false)\n" +
                 "  |> yield(name: \"mean\")";
 
         QueryApi queryApi = influxDBClient.getQueryApi();
-
 
         List<FluxTable> tables = queryApi.query(flux);
         for (FluxTable fluxTable : tables) {
--- src/main/java/iot/views/DashboardView.java
+++ src/main/java/iot/views/DashboardView.java
@@ -2,7 +2,13 @@ package iot.views;
 
 import com.influxdb.query.FluxRecord;
 import com.storedobject.chart.*;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.TabSheet;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import iot.influx.Influx;
@@ -14,45 +20,81 @@ import java.util.List;
 
 @PageTitle("Dashboard")
 @Route(value = "/", layout = MainLayout.class)
-public class DashboardView extends HorizontalLayout {
+public class DashboardView extends VerticalLayout {
 
     public DashboardView() {
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+
+        add(createChart());
+
+    }
+
+    public SOChart createChart() {
         SOChart soChart = new SOChart();
-        soChart.setSize("100%", "500px");
-        String title = "Temperature";
-        soChart.add(new Title(title));
+        soChart.setSize("100%", "550px");
 
         XAxis xAxis = new XAxis(DataType.DATE);
-        xAxis.setDivisions(10);
+        xAxis.setDivisions(4);
         xAxis.setName("Time");
 
         YAxis yAxis = new YAxis(DataType.NUMBER);
-        yAxis.setName("°C");
+        yAxis.setName("Value");
 
         RectangularCoordinate rc = new RectangularCoordinate(xAxis, yAxis);
 
-        /*for ()*/ {
+        /* Temperature */
+        {
             TimeData xValues = new TimeData();
             Data yValues = new Data();
 
             List<FluxRecord> results = Influx.getTemps();
-            if (results != null) {
-                for (FluxRecord result : results) {
-                    xValues.add(LocalDateTime.ofInstant(result.getTime(), ZoneId.systemDefault()));
-                    yValues.add((Number)result.getValueByKey("_value"));
-                }
-            } else {
-                System.out.println("No Chart data.");
-            }
 
-            LineChart lineData = new LineChart(xValues, yValues);
+            LineChart lineData = sampleData(xValues, yValues, results);
             lineData.setName("Temperature");
 
             lineData.plotOn(rc);
             soChart.add(lineData);
         }
+        /* Altitude */
+        {
+            TimeData xValues = new TimeData();
+            Data yValues = new Data();
 
-        add(soChart);
+            List<FluxRecord> results = Influx.getAltitude();
+            LineChart lineData = sampleData(xValues, yValues, results);
+            lineData.setName("Altitude");
+
+            lineData.plotOn(rc);
+            soChart.add(lineData);
+        }
+        /* Pressure */
+        {
+            TimeData xValues = new TimeData();
+            Data yValues = new Data();
+
+            List<FluxRecord> results = Influx.getPressure();
+            LineChart lineData = sampleData(xValues, yValues, results);
+            lineData.setName("Pressure");
+
+            lineData.plotOn(rc);
+            soChart.add(lineData);
+        }
+
+        return soChart;
+    }
+
+    private static LineChart sampleData(TimeData xValues, Data yValues, List<FluxRecord> results) {
+        if (results != null) {
+            for (FluxRecord result : results) {
+                xValues.add(LocalDateTime.ofInstant(result.getTime(), ZoneId.systemDefault()));
+                yValues.add((Number) result.getValueByKey("_value"));
+            }
+        } else {
+            System.out.println("No Chart data.");
+        }
+
+        return new LineChart(xValues, yValues);
     }
 
 }
