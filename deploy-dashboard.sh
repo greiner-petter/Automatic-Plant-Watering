7a8b2bf088fae5184ca0ac53575423798e59b13f
YanSchw <schoessow.yannick@gmail.com>
2024-05-15 17:51:04 +0200

    Update deploy-dashboard.sh

--- deploy-dashboard.sh
+++ deploy-dashboard.sh
@@ -1,3 +1,21 @@
+
+# Installiere notwendige Pakete und entferne die Paketliste
+apt-get update && apt-get install -y --no-install-recommends \
+    curl \
+    sudo \
+    ca-certificates \
+    maven \
+    && rm -rf /var/lib/apt/lists/*
+
+# Installiere nvm (Node Version Manager) und Node.js
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && \
+    . "$NVM_DIR/nvm.sh" && nvm install $NODE_VERSION && nvm use $NODE_VERSION && \
+    npm install -g npm@latest
+
+sudo apt install openjdk-21-jdk
+nvm install 18
+nvm use 18
+
 mvn clean package -Pproduction
-docker build . -t iot-dashboard:latest
-docker run -p 8080:8080 iot-dashboard:latest
+
+sudo docker compose up
